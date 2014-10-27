294d3bb24f04865f0cf6851ac32dfa69de73ee5d
Cédric Vidal <cedric@vidal.biz>
2014-10-27 17:13:18 +0100

    Updating node only if new modules should be installed

--- node/install.sh
+++ node/install.sh
@@ -6,18 +6,20 @@ if [ "$?" -ne "0" ]; then
     exit 1
 fi
 
-echo "Updating node modules"
-npm update -g
-
 # Install Missing modules
 #
 find /usr/local/lib/node_modules -type d -depth 1 | sort | while read path; do basename $path; done > /tmp/installed
 TO_INSTALL=$(comm -13 /tmp/installed modules)
 
 if [ ! -z "${TO_INSTALL}" ]; then
+  echo "Updating node modules before installing new ones"
+  npm update -g
+
   echo $TO_INSTALL | while read module; do
     echo "Installing node module $module"
     npm install -g ${module}
   done
+else
+  echo "All modules are already installed"
 fi
 
