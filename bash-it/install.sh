59c36cfcd6acbae6e4f6ba7798d6c63eb132f2be
Cédric Vidal <cedric@vidal.biz>
2014-10-27 01:38:06 +0100

    Bash it components enabling

---
+++ bash-it/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install Missing Homebrew formulas
+#
+BASHIT_DIR=~/.bash_it
+
+if [ ! -f $BASHIT_DIR/bash_it.sh ]; then
+  echo 'Install Bash it: https://github.com/cedricvidal/bash-it'
+  exit 1
+fi
+
+function install_bashit_piece {
+  piece=$1
+  if [ -f $piece ]; then
+    cat $piece | while read i; do
+      if [ ! -f $BASHIT_DIR/$piece/enabled/$i.aliases.bash ]; then
+        echo "Enabling bash-it $piece $i"
+        ln -s $BASHIT_DIR/$piece/available/$i.aliases.bash $BASHIT_DIR/$piece/enabled/$i.aliases.bash;
+      fi
+    done
+  fi
+}
+
+install_bashit_piece aliases
+install_bashit_piece plugins
+install_bashit_piece completion
+
+source ~/.bash_profile
+
