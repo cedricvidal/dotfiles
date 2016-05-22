1e765bc34e885bce17a9d68e756464121c15a60a
Cédric Vidal <cedric@vidal.biz>
2016-05-22 09:20:32 +0200

    Installing and updating bash-it in installer

--- bash-it/install.sh
+++ bash-it/install.sh
@@ -7,8 +7,8 @@ BASHIT_DIR=~/.bash_it
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f $BASHIT_DIR/bash_it.sh ]; then
-  echo 'Install Bash it: https://github.com/cedricvidal/bash-it'
-  exit 1
+  echo "Installing Bash-it"
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git $BASHIT_DIR
 fi
 
 function install_bashit_piece {
@@ -25,8 +25,11 @@ function install_bashit_piece {
   fi
 }
 
+source ~/.bash_profile
+
+bash-it update
+
 install_bashit_piece aliases aliases
 install_bashit_piece plugins plugin
 install_bashit_piece completion completion
 
-source ~/.bash_profile
