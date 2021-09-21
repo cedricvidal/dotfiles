8da110b6d015c99619fc8a63d691405077f23be2
Cédric Vidal <cedric@vidal.biz>
2021-09-21 18:22:25 +0000

    nvm integration only if installed

--- shell/nvm.sh
+++ shell/nvm.sh
@@ -1,3 +1,5 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+if ! command -v /usr/local/opt/nvm/nvm.sh &> /dev/null; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+fi
